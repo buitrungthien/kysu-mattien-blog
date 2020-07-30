43b5b8b674c325c8114d8fc9b19b786b76f21b70
Thien Bui <thienbt95@gmail.com>
2020-07-30 13:39:13 +0700

    test

--- src/templates/default-blog-post.js
+++ src/templates/default-blog-post.js
@@ -10,6 +10,33 @@ export default ({ data, pageContext }) => {
   const post = data.markdownRemark;
   const { previous, next } = pageContext;
   const { author } = post.frontmatter;
+  useEffect(() => {
+    function windowPopup(url, width, height) {
+      // Calculate the position of the popup so
+      // it’s centered on the screen.
+      var left = window.screen.width / 2 - width / 2,
+        top = window.screen.height / 2 - height / 2;
+
+      window.open(
+        url,
+        '',
+        'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,width=' +
+          width +
+          ',height=' +
+          height +
+          ',top=' +
+          top +
+          ',left=' +
+          left
+      );
+    }
+
+    var facebookIcon = document.querySelector('.facebook > a');
+    facebookIcon.addEventListener('click', function(e) {
+      e.preventDefault();
+      windowPopup(this.href, 500, 500);
+    });
+  }, []);
   return (
     <Layout>
       <SEO
