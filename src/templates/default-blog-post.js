427d1fedf696994732ffe4ca92e1800b9ece9737
Thien Bui <thienbt95@gmail.com>
2020-07-30 13:17:09 +0700

    test share feature

--- src/templates/default-blog-post.js
+++ src/templates/default-blog-post.js
@@ -3,6 +3,7 @@ import { graphql, Link } from 'gatsby';
 import Layout from '../components/layout';
 import Author from '../components/Author';
 import myAvatar from '../images/my-avatar.jpg';
+import SEO from '../components/seo';
 import './styles.scss';
 
 export default ({ data, pageContext }) => {
@@ -11,6 +12,17 @@ export default ({ data, pageContext }) => {
   const { author } = post.frontmatter;
   return (
     <Layout>
+      <SEO
+        title={post.frontmatter.title}
+        description={'THIEN TEST SHARE FEATURE'}
+        meta={[
+          {
+            property: `og:image`,
+            content:
+              'https://cdn.gametv.vn/gtv-photo/GTVNews/2020/01/OG-new-roster.jpg',
+          },
+        ]}
+      />
       <article className="post">
         <div className="post-header">
           <h1 className="post-title">{post.frontmatter.title}</h1>
@@ -27,9 +39,15 @@ export default ({ data, pageContext }) => {
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
         <div className="pre-next-navigator">
           {previous && (
-            <Link to={previous.fields.slug} className="pre-link">{previous.frontmatter.title}</Link>
+            <Link to={previous.fields.slug} className="pre-link">
+              {previous.frontmatter.title}
+            </Link>
           )}
-          {next && <Link to={next.fields.slug} className="next-link">{next.frontmatter.title}</Link>}
+          {next && (
+            <Link to={next.fields.slug} className="next-link">
+              {next.frontmatter.title}
+            </Link>
+          )}
         </div>
       </article>
     </Layout>
